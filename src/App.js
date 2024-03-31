7d3b35ce4c8702457a5f43e36ce5d6b5aa189ded
ceunnseo <eunseo4068@gmail.com>
2024-03-31 21:34:15 +0900

    redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+import {useDispatch, useSelector} from "react-redux"
+import Box from './component/Box';
+import GrandSonBox from './component/GrandSonBox';
 
 function App() {
+  const count = useSelector(state=>state.count)
+  let id = useSelector(state=>state.id)
+  let password = useSelector(state=>state.password)
+  const dispatch = useDispatch();
+  const increase = () => {
+    dispatch({type : "INCREMENT", payload : {num:5}})
+  }
+  const decrease = () => {
+    dispatch({type : "DECREASE", payload : {num:5}})
+  }
+  const login = () => {
+    dispatch({type : "LOGIN", payload : {id : "eunseo", password : 1}})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{id}, {password}</h1>
+      <div>{count}</div>
+      <button onClick = {increase}>increase btn</button>
+      <button onClick = {decrease}>decrease btn</button>
+      <button onClick={login}>Login</button>
+      <Box/>
+      <GrandSonBox/>
     </div>
   );
 }
